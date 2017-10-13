233e695ee9a59f42b8729f68e111580644bd1c51
rajesh khadka <rajeshkhadka1993@gmail.com>
2017-10-14 01:11:01 +0545

    add button one

--- app/src/test/java/com/crushcoder/calculator/ButtonFragmentTest.java
+++ app/src/test/java/com/crushcoder/calculator/ButtonFragmentTest.java
@@ -1,6 +1,11 @@
 package com.crushcoder.calculator;
 
 
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.crushcoder.calculator.support.ResourceLocator;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,22 +13,33 @@ import org.robolectric.RobolectricTestRunner;
 
 import static com.crushcoder.calculator.support.Assert.assertViewIsVisible;
 import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.robolectric.util.FragmentTestUtil.startFragment;
 
 @RunWith(RobolectricTestRunner.class)
 public class ButtonFragmentTest {
 
     private ButtonFragment buttonFragment;
+    private RelativeLayout buttonContainer;
 
     @Before
     public void setUp() throws Exception {
         buttonFragment = ButtonFragment.newInstance();
+        startFragment(buttonFragment);
+        buttonContainer = buttonFragment.getView().findViewById(R.id.calculator_buttons);
     }
 
     @Test
     public void shouldBeNotNull() throws Exception {
         assertNotNull(buttonFragment);
-        startFragment(buttonFragment);
-        assertViewIsVisible(buttonFragment.getView().findViewById(R.id.calculator_buttons));
+        assertViewIsVisible(buttonContainer);
+    }
+
+    @Test
+    public void shouldHaveOneButton() throws Exception {
+        Button buttonOne = buttonFragment.getView().findViewById(R.id.button_1);
+        assertViewIsVisible(buttonOne);
+        assertThat(buttonOne.getText().toString(), equalTo(ResourceLocator.getString(R.string.button_one)));
     }
 }
