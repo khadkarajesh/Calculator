8700f55ed21d3893ad9e1230bd3fb99a0ed98db9
rajesh khadka <rajeshkhadka1993@gmail.com>
2017-10-13 23:26:39 +0545

    add display edit text on display fragment

--- app/src/main/java/com/crushcoder/calculator/DisplayFragment.java
+++ app/src/main/java/com/crushcoder/calculator/DisplayFragment.java
@@ -2,8 +2,24 @@ package com.crushcoder.calculator;
 
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class DisplayFragment extends Fragment {
     public DisplayFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_display, container, false);
+        return view;
+    }
+
+    public static DisplayFragment newInstance() {
+        return new DisplayFragment();
     }
 }
--- app/src/test/java/com/crushcoder/calculator/DisplayFragmentTest.java
+++ app/src/test/java/com/crushcoder/calculator/DisplayFragmentTest.java
@@ -1,19 +1,33 @@
 package com.crushcoder.calculator;
 
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
+import static com.crushcoder.calculator.support.Assert.assertViewIsVisible;
 import static junit.framework.Assert.assertNotNull;
 import static org.robolectric.util.FragmentTestUtil.startFragment;
 
 @RunWith(RobolectricTestRunner.class)
 public class DisplayFragmentTest {
+
+    private DisplayFragment displayFragment;
+
+    @Before
+    public void setUp() throws Exception {
+        displayFragment = DisplayFragment.newInstance();
+        startFragment(displayFragment);
+    }
+
     @Test
     public void shouldNotBeNull() throws Exception {
-        DisplayFragment displayFragment = new DisplayFragment();
-        startFragment(displayFragment);
         assertNotNull(displayFragment);
+    }
+
+    @Test
+    public void shouldHaveDisplay() throws Exception {
+        assertViewIsVisible(displayFragment.getView().findViewById(R.id.calculator_display));
     }
 }
