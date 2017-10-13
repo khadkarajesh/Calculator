361ccb9df837fde92226e36ccba4548b40e07503
rajesh khadka <rajeshkhadka1993@gmail.com>
2017-10-14 00:43:44 +0545

    add button fragment

---
+++ app/src/main/java/com/crushcoder/calculator/ButtonFragment.java
@@ -0,0 +1,13 @@
+package com.crushcoder.calculator;
+
+import android.app.Fragment;
+
+
+public class ButtonFragment extends Fragment {
+    public ButtonFragment() {
+    }
+
+    public static ButtonFragment newInstance() {
+        return new ButtonFragment();
+    }
+}
---
+++ app/src/test/java/com/crushcoder/calculator/ButtonFragmentTest.java
@@ -0,0 +1,27 @@
+package com.crushcoder.calculator;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.robolectric.util.FragmentTestUtil.startFragment;
+
+@RunWith(RobolectricTestRunner.class)
+public class ButtonFragmentTest {
+
+    private ButtonFragment buttonFragment;
+
+    @Before
+    public void setUp() throws Exception {
+        buttonFragment = ButtonFragment.newInstance();
+    }
+
+    @Test
+    public void shouldBeNotNull() throws Exception {
+        assertNotNull(buttonFragment);
+        startFragment(buttonFragment);
+    }
+}
