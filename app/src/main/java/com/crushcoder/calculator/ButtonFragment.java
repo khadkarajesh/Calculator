f11b23cb0a5e21d2351deb2bee873ee070955f75
rajesh khadka <rajeshkhadka1993@gmail.com>
2017-10-14 00:56:24 +0545

    Add button view container

--- app/src/main/java/com/crushcoder/calculator/ButtonFragment.java
+++ app/src/main/java/com/crushcoder/calculator/ButtonFragment.java
@@ -1,6 +1,11 @@
 package com.crushcoder.calculator;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class ButtonFragment extends Fragment {
@@ -9,5 +14,12 @@ public class ButtonFragment extends Fragment {
 
     public static ButtonFragment newInstance() {
         return new ButtonFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_button, container, false);
+        return view;
     }
 }
--- app/src/test/java/com/crushcoder/calculator/ButtonFragmentTest.java
+++ app/src/test/java/com/crushcoder/calculator/ButtonFragmentTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
+import static com.crushcoder.calculator.support.Assert.assertViewIsVisible;
 import static junit.framework.Assert.assertNotNull;
 import static org.robolectric.util.FragmentTestUtil.startFragment;
 
@@ -23,5 +24,6 @@ public class ButtonFragmentTest {
     public void shouldBeNotNull() throws Exception {
         assertNotNull(buttonFragment);
         startFragment(buttonFragment);
+        assertViewIsVisible(buttonFragment.getView().findViewById(R.id.calculator_buttons));
     }
 }
